b87dfc5929b8fda2cce1c409b6325b09d29e67df
Neo <electro.statik@gmail.com>
2016-12-02 22:46:51 +0100

    amelioration du css pour le formulaire de connexion simplifié

--- client/js/psr/main.js
+++ client/js/psr/main.js
@@ -4,12 +4,19 @@
 (function ($, O2) {
     $(function () {
 
+        // Formulaire de connexion
+        var $formLogin = $('<form id="pr-form-login col s2 offset-s1"></form>');
+        var $divRow = $('<div class="row"></div>');
+        var $inputField = $('<div class="input-field col s6 offset-s3"><label for="pseudo">Pseudo</label></div>');
         var $input = $('<input id="pseudo" type="text" class="pr-pseudo" autocomplete="off"/>');
-        var $inputField = $('<div class="input-field col s6 offset-s3 valign"><label for="pseudo">Pseudo</label></div>').append($input);
-        var $btn = $('<a class="pr-login waves-effect waves-light btn col s6 offset-s3"><i class="material-icons right">&#xE163;</i>Démarrer</a>');
-        var $prTop = $('<div class="pr-top row"><div>').append($inputField, $btn);
-        var $body = $('body');
+        var $divBtn = $('<div class="col s6 offset-s3 center-align"></div>');
+        var $btn = $('<button type="submit" class="pr-login waves-effect waves-light btn"><i class="material-icons right">&#xE163;</i>Lancer</button>');
+        var $prTop = $('<div class="pr-top row"><div>');
 
+        $prTop.append($formLogin.append($divRow.append($inputField.append($input), $divBtn.append($btn))));
+
+        // Body et ajout du formulaire au body
+        var $body = $('body');
         $body.append($prTop);
 
 
@@ -27,21 +34,13 @@
         var oChatView = new psr.view.Chat();
         var oChat = new psr.Chat();
 
-        $btn.on('click', function () {
+        $formLogin.on('submit', function (oEvent) {
+            oEvent.preventDefault();
             var $pseudo = $input.val();
             if (oSocketManager.connect($pseudo) === true) {
                 oChat.initView(oChatView);
                 oChat.initSocket(oSocketManager);
                 $prTop.addClass('pr-hidden');
-            }
-        });
-
-        $input.on('keydown', function (oEvent){
-            switch (oEvent.which) {
-                case 13:
-                    $btn.trigger('click');
-                    oEvent.target.value = '';
-                    break;
             }
         });
 
